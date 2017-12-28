ff2d36652e58e5169eb0bb44ff78da78af203778
synical <no@no.no>
2017-12-28 12:16:16 +0000

    Use single function for reading files

--- tracing/ftrace.py
+++ tracing/ftrace.py
@@ -5,30 +5,27 @@ class Ftrace(object):
     def __init__(self):
         self.tracing_path = "/sys/kernel/debug/tracing"
         self.current_tracer_path = self.tracing_path + "/current_tracer"
+        self.tracing_on_path = self.tracing_path + "/current_tracer"
         self.pre_flight_checks()
+
+    def exit_with_error(self, message):
+        print message
+        exit(1)
 
     def pre_flight_checks(self):
         if not os.getuid() == 0:
-            print "This program needs to be executed as root"
-            exit(1)
+            self.exit_with_error("This program needs to be executed as root")
         if not os.path.isdir(self.tracing_path):
-            print """Tracing directory (%s) not found.
-            Is debugfs mounted?""" % (self.tracing_path)
-            exit(1)
-        self.check_nop()
+            self.exit_with_error(("Tracing directory %s not found."
+            "Is debugfs mounted?"
+            % (self.tracing_path)))
+        if self.get_setting(self.current_tracer_path) != "nop":
+            self.exit_with_error(("Tracer currently set."
+                "Please echo nop > %s before running this program"
+                % (self.current_tracer_path)))
 
-    def check_enabled(self):
-        with open(self.tracing_path + "/tracing_on") as f:
-            tracing_on = f.read(1)
-            if tracing_on == 1:
-                print """Tracing currently enabled.
-                Please echo 0 > %s""" % (self.tracing_path+"/tracing_on")
+    def get_setting(self, path):
+        with open(path) as f:
+            setting= f.readline().strip()
+        return setting
 
-    def check_nop(self):
-        with open(self.current_tracer_path) as f:
-            current_tracer = f.readline().strip()
-            if current_tracer != "nop":
-                print """Tracer currently set to %s.
-                Please echo nop > %s before running this program""" \
-                % (current_tracer, self.current_tracer_path)
-                exit(1)
