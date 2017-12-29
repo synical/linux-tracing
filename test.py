12e552a5ae220d966cdb69100a4f727829807655
synical <no@no.no>
2017-12-29 14:25:19 +0000

    Basic blk enable/disable functionality

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
+from time import sleep
 from tracing import ftrace
 
 ft = ftrace.Ftrace()
+ft.enable_block_tracing()
+sleep(10)
+print ft.get_trace_data()
+ft.disable_block_tracing()
--- tracing/ftrace.py
+++ tracing/ftrace.py
@@ -5,8 +5,18 @@ class Ftrace(object):
     def __init__(self):
         self.tracing_path = "/sys/kernel/debug/tracing"
         self.current_tracer_path = self.tracing_path + "/current_tracer"
+        self.trace_data_path = self.tracing_path + "/trace"
         self.tracing_on_path = self.tracing_path + "/current_tracer"
+        self.block_trace_enable_path = self.tracing_path + "/events/block/enable"
         self.pre_flight_checks()
+
+    def disable_block_tracing(self):
+        self.set_value("0", self.block_trace_enable_path)
+        self.set_value("nop", self.current_tracer_path)
+
+    def enable_block_tracing(self):
+        self.set_value("1", self.block_trace_enable_path)
+        self.set_value("blk", self.current_tracer_path)
 
     def exit_with_error(self, message):
         print message
@@ -16,11 +26,11 @@ class Ftrace(object):
         if not os.getuid() == 0:
             self.exit_with_error("This program needs to be executed as root")
         if not os.path.isdir(self.tracing_path):
-            self.exit_with_error(("Tracing directory %s not found."
+            self.exit_with_error(("Tracing directory %s not found. "
             "Is debugfs mounted?"
             % (self.tracing_path)))
         if self.get_setting(self.current_tracer_path) != "nop":
-            self.exit_with_error(("Tracer currently set."
+            self.exit_with_error(("Tracer currently set. "
                 "Please echo nop > %s before running this program"
                 % (self.current_tracer_path)))
 
@@ -29,3 +39,11 @@ class Ftrace(object):
             setting= f.readline().strip()
         return setting
 
+    def get_trace_data(self):
+        with open(self.trace_data_path) as f:
+            data = f.read(4096)
+        return data
+
+    def set_value(self, value, path):
+        with open(path, "w") as f:
+            f.write(value)
