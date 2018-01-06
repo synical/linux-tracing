202d4d826c9de0371887b984055d6e7c69fad63e
synical <no@no.no>
2018-01-06 21:45:38 +0000

    Add check for kernel option

--- tracing/ftrace.py
+++ tracing/ftrace.py
@@ -1,5 +1,7 @@
 import os
 
+from platform import release
+
 class Ftrace(object):
 
     def __init__(self):
@@ -9,6 +11,12 @@ class Ftrace(object):
         self.tracing_on_path = self.tracing_path + "/current_tracer"
         self.block_trace_enable_path = self.tracing_path + "/events/block/enable"
         self.pre_flight_checks()
+
+    def check_ftrace_configured(self):
+        with open("/boot/config-%s" % (release())) as f:
+            if "CONFIG_FTRACE=y" in f.read():
+                return True
+            return False
 
     def disable_block_tracing(self):
         self.set_value("0", self.block_trace_enable_path)
@@ -23,6 +31,8 @@ class Ftrace(object):
         exit(1)
 
     def pre_flight_checks(self):
+        if not self.check_ftrace_configured():
+            self.exit_with_error("Kernel not compiled with CONFIG_FTRACE!")
         if not os.getuid() == 0:
             self.exit_with_error("This program needs to be executed as root")
         if not os.path.isdir(self.tracing_path):
