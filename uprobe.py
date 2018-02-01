00ec916207f99ee552f365b486024e3400f94c0e
synical <sjohnsondot@gmail.com>
2018-02-01 04:08:42 +0000

    Don't check for valid uprobe, catch exception instead

--- uprobe.py
+++ uprobe.py
@@ -24,20 +24,11 @@ class Uprobe(object):
         print(message)
         exit(1)
 
-    def parse_uprobe(self):
-        error_message = "Invalid uprobe, must be of format 'p:<name> <file>:<addr> <args...>'!" 
-        try:
-            uprobe_string = self.uprobe_event.split(" ")
-            uprobe_name = uprobe_string[0].split(":")
-            uprobe_entry = uprobe_string[1].split(":")
-        except IndexError:
-            self.exit_with_error(error_message)
-        if self.uprobe_event[0] != "p" or len(uprobe_name) != 2 or len(uprobe_entry) !=2:
-            self.exit_with_error(error_message)
-
     def trace_entry(self):
-        self.parse_uprobe()
-        self.ft.set_uprobe_event(self.uprobe_event)
+        try:
+            self.ft.set_uprobe_event(self.uprobe_event)
+        except IOError:
+            self.exit_with_error("Invalid uprobe '%s'" % (self.uprobe_event))
         self.ft.enable_uprobe_tracing()
         for line in self.ft.get_trace_snapshot():
             if line[0] != "#":
