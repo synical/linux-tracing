6073834cff4b434775001de249cefb756820ca39
synical <sjohnsondot@gmail.com>
2018-02-01 15:21:19 +0000

    Add arg for specifying sample period

--- uprobe.py
+++ uprobe.py
@@ -21,7 +21,7 @@ class Uprobe(object):
         print(message)
         exit(1)
 
-    def trace_entry(self, pid=None):
+    def trace_entry(self, pid=None, period=1):
         try:
             if pid:
                 pid = "common_pid == %s" % (pid)
@@ -29,6 +29,7 @@ class Uprobe(object):
             self.ft.enable_uprobe_tracing(uprobe_filter=pid)
         except IOError:
             self.exit_with_error("Invalid uprobe '%s'" % (self.uprobe_event))
+        sleep(float(period))
         for line in self.ft.get_trace_snapshot():
             if line[0] != "#":
                 print line,
@@ -38,6 +39,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-u", "--uprobe", action="store", dest="uprobe", required=True, help="uprobe entry point")
     parser.add_argument("-p", "--pid", action="store", dest="pid", help="pid to filter on")
+    parser.add_argument("-s", "--sample", action="store", dest="sample", default=1, help="Seconds to sample for")
     return parser.parse_args()
 
 def main():
@@ -46,7 +48,7 @@ def main():
         print "Kernel version must be 4.0 or greater!"
         exit(1)
     up = Uprobe(args.uprobe)
-    up.trace_entry(pid=args.pid)
+    up.trace_entry(pid=args.pid, period=args.sample)
 
 if __name__ == '__main__':
     main()
