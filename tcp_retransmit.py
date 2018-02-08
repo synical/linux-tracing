54747bf945c985e94ac3c740aac1c360518ce460
synical <sjohnsondot@gmail.com>
2018-02-08 15:36:23 +0000

    Add tcp_retransmit skeleton

---
+++ tcp_retransmit.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import argparse
+
+from tracing import ftrace
+
+"""
+  TODO
+    - Latency of retransmit
+    - Stacktrace leading to retransmit
+    - Count of retransmits (by process)
+"""
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
