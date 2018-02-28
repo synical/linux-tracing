acfd8fa16b40fd2636f46784e8fff7bda77c703b
synical <sjohnsondot@gmail.com>
2018-02-28 15:21:05 +0000

    Add skeleton for function tracing

---
+++ functrace.py
@@ -0,0 +1,12 @@
+from tracing.ftrace import function_graph,function
+
+class FuncTrace(object):
+
+    def __init__():
+        pass
+
+def main():
+    pass
+
+def __main__():
+    pass
---
+++ tracing/ftrace/function_graph.py
@@ -0,0 +1,12 @@
+from ftrace import Ftrace
+
+class FunctionGraph(Ftrace):
+
+    def __init__():
+        Ftrace.__init__(self)
+
+    def disable_tracing():
+        pass
+
+    def enable_tracing():
+        pass
