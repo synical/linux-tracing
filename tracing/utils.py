deb0073b113d083250e1778ad76e6154d97df5cf
synical <sjohnsondot@gmail.com>
2018-03-22 01:55:01 +0000

    Basic histogram

--- tracing/utils.py
+++ tracing/utils.py
@@ -12,3 +12,21 @@ def compute_distribution(stats):
         return distribution
     except ValueError:
         return {}
+
+def compute_histogram(stats):
+    counts = {x: stats.count(x) for x in set(stats)}
+    lowest = min(stats)
+    highest = max(stats)
+    
+    i=0
+    while True:
+        if i == 0:
+            print "0\t->\t1\t0"
+            i=2
+        first = i
+        second = ((i*2)-1)
+        num_hits = sum([counts[k] for k in counts.keys() if k >= first and k <= second])
+        print "%d\t->\t%d\t%d" % (first, second, num_hits)
+        i*=2
+        if i > highest:
+            break
