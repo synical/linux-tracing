367f8df2af19f2a08470409ce0d4e7b81cfdc734
synical <sjohnsondot@gmail.com>
2018-03-23 14:32:02 +0000

    Add graphical distribution representation

--- tracing/utils.py
+++ tracing/utils.py
@@ -15,19 +15,28 @@ def compute_distribution(stats):
 
 def compute_histogram(stats, unit="ms"):
     counts = {x: stats.count(x) for x in set(stats)}
+    stats_len = len(stats)
     lowest = min(stats)
     highest = max(stats)
+    bar_length = float(10)
     
-    i=0
     print "Value(%s)\t\tCount\t\tDistribution" % (unit)
+    if lowest == 0:
+        print "0\t->\t1\t0"
+        i = 1
+    else:
+        i = lowest
+
     while True:
-        if i == 0:
-            print "0\t->\t1\t0"
-            i=2
         first = i
-        second = ((i*2)-1)
+        if first == 1:
+            second = 2
+        else:
+            second = ((i*2)-1)
         num_hits = sum([counts[k] for k in counts.keys() if k >= first and k <= second])
-        print "%d\t->\t%d\t%d" % (first, second, num_hits)
+        frequency = (float(num_hits)/float(stats_len) * 100) / bar_length
+        bar = "#" * int(frequency)
+        print "%d\t->\t%d\t%d\t\t%s" % (first, second, num_hits, bar)
         i*=2
         if i > highest:
             break
