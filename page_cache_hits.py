1c2e490d222380df3cecdf406950c7c0d5529732
synical <sjohnsondot@gmail.com>
2018-03-28 00:13:16 +0000

    Skeleton for page_cache_hits

---
+++ page_cache_hits.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import argparse
+
+from tracing import ftrace
+
+"""
+  TODO
+    - Hit rate system wide
+    - Hit rate on pid
+"""
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
